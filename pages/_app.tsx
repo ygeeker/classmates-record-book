67f5aa9e130c6cd98c07a922c7ef28f4e46ec8bc
Justin Sun <i@jsun969.cn>
2022-03-04 22:54:37 +0800

    添加并初始化Ant Design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import 'antd/dist/antd.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
