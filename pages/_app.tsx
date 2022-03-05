21ebe81860a23a3967ff149a4731e51940c03358
Justin Sun <i@jsun969.cn>
2022-03-05 10:36:08 +0800

    添加并初始化tailwind.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import type { AppProps } from 'next/app';
 import 'antd/dist/antd.css';
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
