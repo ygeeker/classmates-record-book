3bc556b2d35fea79b402449c26a13c03595ffa0d
Justin Sun <i@jsun969.cn>
2022-03-06 09:47:00 +0800

    增加大屏幕grid为四列

--- pages/index.tsx
+++ pages/index.tsx
@@ -76,7 +76,7 @@ const Index: NextPage<IndexProps> = ({ students }) => {
     );
   } else {
     return (
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {students.map((student) => (
           <Badge.Ribbon
             key={student.id}
