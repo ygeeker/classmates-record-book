8914371ab603ffb267fa28b797466052b2807391
Justin Sun <i@jsun969.cn>
2022-06-16 22:45:33 +0800

    地图信息仅显示有人的省份并显示人数

--- src/components/map/InfoPanel.tsx
+++ src/components/map/InfoPanel.tsx
@@ -5,16 +5,26 @@ import { Collapse } from 'antd';
 const InfoPanel: React.FC<{ students: StudentData[] }> = ({ students }) => {
   return (
     <Collapse>
-      {PROVINCE_POST_CODE_LIST.map(({ code, province }) => (
-        <Collapse.Panel header={province} key={code}>
-          {students
-            .filter(({ provincePostCode }) => provincePostCode === code)
-            .map(
-              (student) =>
-                `${student.class}班 ${student.name} ${student.school}`,
-            )}
-        </Collapse.Panel>
-      ))}
+      {PROVINCE_POST_CODE_LIST.filter(({ code }) =>
+        students.map(({ provincePostCode }) => provincePostCode).includes(code),
+      ).map(({ code, province }) => {
+        const filteredStudents = students.filter(
+          ({ provincePostCode }) => provincePostCode === code,
+        );
+
+        return (
+          <Collapse.Panel
+            header={`${province} ${filteredStudents.length}人`}
+            key={code}
+          >
+            {filteredStudents.map((student) => (
+              <div key={student.id}>
+                {`${student.class}班 ${student.name} ${student.school}`}
+              </div>
+            ))}
+          </Collapse.Panel>
+        );
+      })}
     </Collapse>
   );
 };
