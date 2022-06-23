cc4c7f664aea5919a0c09154a03611dbcccee8fe
RiverTwilight <wrj2014@126.com>
2022-06-23 08:07:23 +0000

    style: 更改头部

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,4 @@
 import { useLayoutMenuValue } from '../contexts/layout-menu';
-import {
-  FormOutlined,
-  GlobalOutlined,
-  HomeOutlined,
-  LoginOutlined,
-} from '@ant-design/icons';
-import { Layout as AntdLayout, Menu, Typography } from 'antd';
 import Head from 'next/head';
 import Link from 'next/link';
 import React from 'react';
@@ -24,12 +17,12 @@ const Layout: React.FC = ({ children }) => {
       <nav className="border fixed split-nav">
         <div className="nav-brand">
           <h3>
-            <a href="#">Get PaperCSS</a>
+            <a href="#">{process.env.NEXT_PUBLIC_TITLE_NAME}同学录</a>
           </h3>
         </div>
         <div className="collapsible">
           <input id="collapsible1" type="checkbox" name="collapsible1" />
-          <label for="collapsible1">
+          <label htmlFor="collapsible1">
             <div className="bar1"></div>
             <div className="bar2"></div>
             <div className="bar3"></div>
@@ -37,59 +30,25 @@ const Layout: React.FC = ({ children }) => {
           <div className="collapsible-body">
             <ul className="inline">
               <li>
-                <a href="#">Documentation</a>
+                <Link href="/">主页</Link>
               </li>
               <li>
-                <a href="#">About</a>
+                <Link href="/register">登记</Link>
               </li>
               <li>
-                <a href="#">Github</a>
+                <Link href="/map">地图</Link>
               </li>
             </ul>
           </div>
         </div>
       </nav>
 
-      <AntdLayout className="min-h-screen">
-        <AntdLayout.Header>
-          <Link href="/">
-            <a className="text-white float-left">
-              {process.env.NEXT_PUBLIC_TITLE_NAME}同学录
-            </a>
-          </Link>
-          <Menu
+      {/* <Menu
             theme="dark"
             mode="horizontal"
             className="float-right w-4 sm:w-auto"
             selectedKeys={[layoutMenu]}
-          >
-            <Menu.Item key="index" icon={<HomeOutlined />}>
-              <Link href="/">主页</Link>
-            </Menu.Item>
-            <Menu.Item key="register" icon={<FormOutlined />}>
-              <Link href="/register">登记</Link>
-            </Menu.Item>
-            <Menu.Item key="map" icon={<GlobalOutlined />}>
-              <Link href="/map">地图</Link>
-            </Menu.Item>
-            <Menu.Item key="admin" icon={<LoginOutlined />}>
-              <Link href="/admin/login">后台</Link>
-            </Menu.Item>
-          </Menu>
-        </AntdLayout.Header>
-        <AntdLayout.Content>
-          <div className="bg-white p-6 m-5">{children}</div>
-        </AntdLayout.Content>
-        <AntdLayout.Footer className="text-center">
-          Github:{' '}
-          <Typography.Link
-            href="https://github.com/jsun969/classmates-record-book"
-            target="_blank"
-          >
-            jsun969/classmates-record-book
-          </Typography.Link>
-        </AntdLayout.Footer>
-      </AntdLayout>
+          > */}
     </>
   );
 };
